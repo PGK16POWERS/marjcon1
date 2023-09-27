bfeef6603839e31ccd46443fb071f81272f03355
programmer Kartier <progrmmerKartier@gmail.com>
2023-09-27 20:43:25 +0200

    lots done

--- node.js
+++ node.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 
 
+app.use(express.static("public"));
 
+app.get("/", (req,res)=> {
+    res.sendFile(path.join(__dirname,"public", "home.html"))
+})
 
 
 
